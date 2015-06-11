6e3ed3e8f8ad204660102b939220edc00e0e7ad4
jiorugu <eragon1@freemail.hu>
2015-06-11 19:54:52 +0200

    Achievement text added, currently no function - only looking good xD

--- src/layers/menu/AchievementsMenuLayer.js
+++ src/layers/menu/AchievementsMenuLayer.js
@@ -17,11 +17,25 @@ var AchievementsMenuLayer = cc.Layer.extend({
 		//TODO: Change to relative positions, generate menuItems dynamically
 		cc.MenuItemFont.setFontSize(60);
 
+		var achievement1 = new cc.MenuItemFont("Away from Keyboard \n - 15 Minuten überhaupt nichts getan");
+		achievement1.setPosition(new cc.p(cc.winSize.width/2,cc.winSize.height/2+150));
+		achievement1.setFontSize(30);
+		
+		var achievement2 = new cc.MenuItemFont("No Game no Life \n - 10 Stunden durchgehend gespielt");
+		achievement2.setPosition(new cc.p(cc.winSize.width/2,cc.winSize.height/2+75));
+		achievement2.setFontSize(30);
+		
+		var achievement3 = new cc.MenuItemFont("Furioser Stuhlgang \n - 50 Levels nacheinander geschafft");
+		achievement3.setPosition(new cc.p(cc.winSize.width/2,cc.winSize.height/2-0));
+		achievement3.setFontSize(30);
+
+		
+		//back to main menu
 		var menuItemReturn = new cc.MenuItemFont("Return", this.returnToMainMenu);
 		menuItemReturn.setPosition(new cc.p(cc.winSize.width/2,cc.winSize.height/2-100));
 
 		//Applying Items to menu to scene
-		var menu = new cc.Menu(menuItemReturn);
+		var menu = new cc.Menu(achievement1,achievement2,achievement3,menuItemReturn);
 		menu.setPosition(new cc.p(0,0));
 
 		this.addChild(menu);
